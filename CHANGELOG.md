31b7822e6a5e52570ef0d8361c0569555008950d
Sandro Costa <sandro.costa@fccn.pt>
2021-08-04 15:42:58 +0100

    Fix: Mobile menu spacing fixes and height issues #1 Feature: TOC index follows page scroll #2 Refactor: toc.js now uses const instead of var

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Theme changelog
 
+## 04/08/2021
+* Enhanced side menu for table of content pages
+* Fixed the mobile menu height
+* Fixed line-height on mobile viewport
+
 ## 03/08/2021
 * Add colors by area
 * Add areas to the _meta.md file.
--- js/toc.js
+++ js/toc.js
@@ -1,14 +1,15 @@
+const tocContainer = document.getElementById('page-toc-container');
+
 function generatePageTOC() {
-    var tocContainer = document.getElementById('page-toc-container');
     document.querySelectorAll('article h2').forEach(function(el, index) {
       // Insert a marker since anchor links breaks with css position sticky
       // sticky headers upwards are stacked at the top of the viewport
       // and therefore cannot be jumped to since they are already visible
       // Instead inject a hidden element to use as a scroll marker
       el.insertAdjacentHTML('beforebegin', '<i id="page-toc-'+index+'" class="page-toc-scroll-marker sr-only">Marcador de capítulo</i>');
-      var newTocNavItem = document.createElement('li');
-      var newTocNavItemLink = document.createElement('a');
-      var newTocNavItemText = document.createTextNode(el.textContent);
+      const newTocNavItem = document.createElement('li');
+      const newTocNavItemLink = document.createElement('a');
+      const newTocNavItemText = document.createTextNode(el.textContent);
       newTocNavItemLink.setAttribute('href', '#page-toc-' + index);
       newTocNavItemLink.appendChild(newTocNavItemText);
       newTocNavItem.appendChild(newTocNavItemLink);
