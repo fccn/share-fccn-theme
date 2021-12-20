db47b3dff0c1d10a971439742f24f7565f4a2144
CORP\glopes <goncalo.lopes@fccn.pt>
2021-12-20 16:07:02 +0000

    Fix: no eventlistener added to toggle when darkmode is disabled

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,22 +1,24 @@
 const toggle = document.querySelector('.theme-switcher input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-toggle.addEventListener('change', function(event){
-    if(event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }
-}, false);
-
-
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggle.checked = true;
+// Ensure darkmode is activated
+if (toggle) {
+    toggle.addEventListener('change', function(event){
+        if(event.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }
+    }, false);
+    
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+    
+        if (currentTheme === 'dark') {
+            toggle.checked = true;
+        }
     }
 }
 
